df705e1b5efab164a00fbd4e203b791c8c36bd5b
Abu Raihan <raihanahmed01973@gmail.com>
2022-09-09 13:09:44 +0600

    done everything without align legend item

---
+++ chart.js
@@ -0,0 +1,47 @@
+const dataDoughnut = {
+  labels: ["possitive", "negetive", "unsure"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [23, 20, 6],
+
+      backgroundColor: ["#9791FF", "#FF5959", "lightgray"],
+      borderColor: ["#9791FF", "#FF5959", "lightgray"],
+      cutout: "35",
+    },
+  ],
+};
+// config
+const doughnutConfig = {
+  type: "doughnut",
+  data: dataDoughnut,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        id: "legendMargin",
+        position: "left",
+        align: "start",
+        fontSize: "14",
+        display: "flex",
+        labels: {
+          pointStyle: "circle",
+          usePointStyle: true,
+          padding: 12,
+        },
+      },
+    },
+  },
+};
+
+// data redering
+const missionDoughnut = new Chart(
+  document.getElementById("doughnut"),
+  doughnutConfig
+);
+
+const vissionDoughnut = new Chart(
+  document.getElementById("vissionDoughnut"),
+  doughnutConfig
+);
